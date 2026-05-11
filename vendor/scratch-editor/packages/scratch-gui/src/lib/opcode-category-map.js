9d256750c08e8dfa5b23c42c8e5faa3badfd74db
Josenil01 <josenil01@github.com>
2026-05-11 11:38:09 -0300

    feat: suporte a blocos de extensao (pen/music/etc) no sistema de atividades
    - opcode-category-map.js: adiciona mapeamento de prefixos de extensao (pen, music,
      text2speech, translate, videoSensing, microbit, makeymakey, boost, wedo2, ev3,
      gdxfor, faceSensing) para que allowedCategories filtre o toolbox corretamente
    - mock-activity.json (src + build): adiciona step demonstrativo com blocos pen
      (pen_penDown, pen_penUp, pen_clear) incluindo allowedCategories com 'pen'

--- vendor/scratch-editor/packages/scratch-gui/src/lib/opcode-category-map.js
+++ vendor/scratch-editor/packages/scratch-gui/src/lib/opcode-category-map.js
@@ -8,6 +8,7 @@ const getCategoryForOpcode = function (opcode) {
     if (!opcode) return null;
     const prefix = opcode.split('_')[0];
     const map = {
+        // Core categories
         motion: 'motion',
         looks: 'looks',
         sound: 'sound',
@@ -16,7 +17,21 @@ const getCategoryForOpcode = function (opcode) {
         sensing: 'sensing',
         operator: 'operators',
         data: 'variables',
-        procedures: 'myBlocks'
+        procedures: 'myBlocks',
+        // Extension categories
+        pen: 'pen',
+        music: 'music',
+        text2speech: 'text2speech',
+        translate: 'translate',
+        speech2text: 'speech2text',
+        videoSensing: 'videoSensing',
+        microbit: 'microbit',
+        makeymakey: 'makeymakey',
+        boost: 'boost',
+        wedo2: 'wedo2',
+        ev3: 'ev3',
+        gdxfor: 'gdxfor',
+        faceSensing: 'faceSensing'
     };
     return map[prefix] || null;
 };
